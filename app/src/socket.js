11ff937e8d1f4f82b441cbafe40019c681881fab
Yuvapriya <yuvapriya1267@gmail.com>
2026-05-30 23:12:34 +0530

    Frontend Deployment Configuration

--- app/src/socket.js
+++ app/src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 let socket;
 
 export const connectSocket = () => {
-  socket = io("http://localhost:5000", {
+  socket = io(import.meta.env.VITE_API_URL, {
     transports: ["websocket"],
     auth: {
       token: localStorage.getItem("token"),
