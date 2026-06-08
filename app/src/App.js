8ef484c68378c4b954ae6ad640b374741c12e528
Yuvapriya <yuvapriya1267@gmail.com>
2026-06-08 22:33:48 +0530

    Socket connection correction

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,8 @@ import Login from "./pages/login";
 import Register from "./pages/register";
 import Home from "./pages/home";
 import Buddy from "./pages/buddy";
+import {connectSocket} from socket.js;
+import {useEffect} from "react";
 
 
 function App(){
